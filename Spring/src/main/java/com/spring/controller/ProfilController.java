9b3d9e05f0e9f011af7886b4bba957b0220820d6
Owen2963 <130049264+Owen2963@users.noreply.github.com>
2025-11-30 03:38:36 +0100

    Update ProfilController.java

--- Spring/src/main/java/com/spring/controller/ProfilController.java
+++ Spring/src/main/java/com/spring/controller/ProfilController.java
@@ -8,8 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;      // ✔ IMPORT IMPORTANT
-import org.springframework.web.bind.annotation.RequestParam; // ✔ IMPORT IMPORTANT
+import org.springframework.web.bind.annotation.PostMapping;      // IMPORT IMPORTANT
+import org.springframework.web.bind.annotation.RequestParam; // IMPORT IMPORTANT
 
 
 @Controller
